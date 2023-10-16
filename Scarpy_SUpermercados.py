c209f4756881b4c4d916111bbf733d88aaefeb82
manuelpalomeque <pjonathanmanuel@hotmail.com>
2023-10-16 19:21:27 -0300

    Soluciono el inconveniente entre los miles y cientos de los precios, mediante la impementacion d euna estructura condicional

--- Scarpy_SUpermercados.py
+++ Scarpy_SUpermercados.py
@@ -26,22 +26,21 @@ fechaActual = fecha.strftime(fDiaMesAño)
 class ProductosSpider(scrapy.Spider):
     name = 'Canasta'
     start_urls = [
-        # las paginas deben estar en comillas dobles, ya que sino da error 404
         "https://www.hiperlibertad.com.ar/pan-frances-x-500-g/p",
-        "https://www.hiperlibertad.com.ar/galleta-la-providencia-crackers-505-gr/p",
-        "https://www.hiperlibertad.com.ar/galletas-terrabusi-variedad-mix-x-170-gr-2/p",
-        "https://www.hiperlibertad.com.ar/arroz-parboil-gallo-oro-x-500-gr/p",
+        "https://www.hiperlibertad.com.ar/galletita-cracker-la-providencia-clasica-x303g/p",
+        "https://www.hiperlibertad.com.ar/galletas-terrabusi-variedad-dorada-x-300-gr/p",
+        "https://www.hiperlibertad.com.ar/arroz-parboil-taeq-240gr/p",
         "https://www.hiperlibertad.com.ar/harina-de-trigo-morixe-000-1-kg/p",
-        "https://www.hiperlibertad.com.ar/fideos-tallarines-lucchetti-500-gr/p",
+        "https://www.hiperlibertad.com.ar/fideos-tallarines-n7-matarazzo-x-500-gr/p",
         "https://www.hiperlibertad.com.ar/papa-cepillada-x-1-kg/p",
         "https://www.hiperlibertad.com.ar/batata-x-500-g/p",
         "https://www.hiperlibertad.com.ar/azucar-azucel-1-kilo/p",
         "https://www.hiperlibertad.com.ar/mermelada-dulcor-durazno-frasco-454-gr/p",
         "https://www.hiperlibertad.com.ar/lentejas-taeq-200gr/p",
-        "https://www.hiperlibertad.com.ar/arvejas-inca-lata-x-350-gr/p",
-        "https://www.hiperlibertad.com.ar/paquete-de-acelga-x-un/p",
+        "https://www.hiperlibertad.com.ar/arvejas-montenevi-x-340-gr/p",
+        "https://www.hiperlibertad.com.ar/acelga-congelada-green-life-500-g/p",
         "https://www.hiperlibertad.com.ar/cebolla-x-500-g/p",
-        "https://www.hiperlibertad.com.ar/lechuga-mantecosa-x-500-g/p",
+        "https://www.hiperlibertad.com.ar/lechuga-arrepollada-x-500g/p",
         "https://www.hiperlibertad.com.ar/tomate-perita-x-500g/p",
         "https://www.hiperlibertad.com.ar/calabacin-anquito-fraccionado-x-700gr-1-2-calabacin/p",
         "https://www.hiperlibertad.com.ar/mandarina-x-1kg/p",
@@ -50,14 +49,14 @@ class ProductosSpider(scrapy.Spider):
         "https://www.hiperlibertad.com.ar/pollo-fresco-x-un-aprox-2-5kg/p",
         "https://www.hiperlibertad.com.ar/pulpa-de-jamon-de-cerdo-x-500-g/p",
         "https://www.hiperlibertad.com.ar/milanesa-de-bola-de-lomo-de-novillito-x-500-g/p",
-        "https://www.hiperlibertad.com.ar/huevo-color-carnave-carton-x-12-unidades/p",
+        "https://www.hiperlibertad.com.ar/huevo-blanco-carnave-carton-x-12-unidades/p",
         "https://www.hiperlibertad.com.ar/leche-entera-c-calcio-la-lacteo-larga-vida-1-l/p",
         "https://www.hiperlibertad.com.ar/manteca-tonadita-200gr/p",
-        "https://www.hiperlibertad.com.ar/aceite-pureza-girasol-900-cc/p",
+        "https://www.hiperlibertad.com.ar/aceite-de-girasol-alsamar-x-900-ml/p",
         "https://www.hiperlibertad.com.ar/brahma-chopp-can-4x6-473cc/p",
-        "https://www.hiperlibertad.com.ar/sal-fina-dos-anclas-500-gr-2/p",
+        "https://www.hiperlibertad.com.ar/sal-fina-dos-anclas-250-gr/p",
         "https://www.hiperlibertad.com.ar/mayonesa-hellmanns-clasica-x-475-gr/p",
-        "https://www.hiperlibertad.com.ar/cafe-molido-bonafide-sensaciones-torrado-suave-500-gr/p",
+        "https://www.hiperlibertad.com.ar/cafe-molido-bonafide-sensaciones-torrado-intenso-500-gr/p",
         "https://www.hiperlibertad.com.ar/yerba-playadito-suave-bcp-1kg/p",
     ]
 
@@ -80,16 +79,20 @@ class ProductosSpider(scrapy.Spider):
         # Utilizando expresiones CSS
         nombre_producto = response.css('span.vtex-store-components-3-x-productBrand::text').get()
         # El precio esta dividido en 3 span diferentes (miles, cientos, centavos)
-        precio_producto_miles = response.css("span.vtex-product-price-1-x-currencyInteger::text").get()
-        precio_producto_cientos = response.css("span.vtex-product-price-1-x-currencyInteger::text").get()
+        precio_producto_miles = response.css("span.vtex-product-price-1-x-currencyInteger::text").getall()
+        #precio_producto_cientos = response.css("span.vtex-product-price-1-x-currencyInteger::text").get()
         precio_producto_decimales =  response.css("span.vtex-product-price-1-x-currencyFraction::text").get()
-
+        if len(precio_producto_miles[0])> 1:
+            precio = precio_producto_miles[1]
+        else:
+            precio = f'{precio_producto_miles[0]}.{precio_producto_miles[1]}'
         yield {
             "fecha": fechaActual,
             "hora": horaActual,
             "Nombre Producto": nombre_producto,
             # El precio esta dividido en 3 span diferentes (miles, cientos, centavos), por eso los contateno:
-            "Precio": f"{precio_producto_miles}.{precio_producto_cientos},{precio_producto_decimales}"
+            "Precio": precio,
+            #"Precio": f"{precio},{precio_producto_decimales}"
             # No logro diferenciar los divs de miles de lo que corresponde a los cientos
             #"pagina": ProductosSpider.start_urls,  ME TRAE EL LISTADO COMPLETO DE PAGINAS NO LA QUE USA EN EL MOMENTO
         }
