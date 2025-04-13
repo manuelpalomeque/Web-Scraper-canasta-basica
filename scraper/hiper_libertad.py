0e22fc46085fa38f2c0ef2a912a1da601ccf2c00
Jonathan Manuel Palomeque <87273052+manuelpalomeque@users.noreply.github.com>
2025-04-13 12:19:33 -0300

    etiqueta meta

--- scraper/hiper_libertad.py
+++ scraper/hiper_libertad.py
@@ -28,38 +28,30 @@ fechaActual = fecha.strftime(fDiaMesAño)
 class ProductosSpider(scrapy.Spider):
     name = 'Canasta'
     start_urls = [
-        "https://www.hiperlibertad.com.ar/pan-frances-x-500-g/p",
-        "https://www.hiperlibertad.com.ar/galletita-cracker-la-providencia-clasica-x303g/p",
-        "https://www.hiperlibertad.com.ar/galletas-terrabusi-variedad-dorada-x-300-gr/p",
-        "https://www.hiperlibertad.com.ar/arroz-parboil-taeq-240gr/p",
-        "https://www.hiperlibertad.com.ar/harina-de-trigo-morixe-000-1-kg/p",
-        "https://www.hiperlibertad.com.ar/fideos-tallarines-n7-matarazzo-x-500-gr/p",
+        "https://www.hiperlibertad.com.ar/pan-baguete-x-unidad-2/p",
+        "https://www.hiperlibertad.com.ar/galletitas-sandwich-mediatarde-x3-107-g/p",
+        "https://www.hiperlibertad.com.ar/galletitas-surtido-diversi-n-en-bolsa-400-g/p",
+        "https://www.hiperlibertad.com.ar/arroz-blanco-largo-fino-molinos-ala-1-kg/p",
+        "https://www.hiperlibertad.com.ar/harina-000-favorita-con-vitaminas-x-1-kg/p",
         "https://www.hiperlibertad.com.ar/papa-cepillada-x-1-kg/p",
         "https://www.hiperlibertad.com.ar/batata-x-500-g/p",
-        "https://www.hiperlibertad.com.ar/azucar-azucel-1-kilo/p",
-        "https://www.hiperlibertad.com.ar/mermelada-dulcor-durazno-frasco-454-gr/p",
-        "https://www.hiperlibertad.com.ar/lentejas-taeq-200gr/p",
-        "https://www.hiperlibertad.com.ar/arvejas-montenevi-x-340-gr/p",
-        "https://www.hiperlibertad.com.ar/acelga-congelada-green-life-500-g/p",
+        "https://www.hiperlibertad.com.ar/azucar-ledesma-clasica-1-kg/p",
+        "https://www.hiperlibertad.com.ar/dulce-de-leche-clasico-ilolay-400-gr/p",
         "https://www.hiperlibertad.com.ar/cebolla-x-500-g/p",
-        "https://www.hiperlibertad.com.ar/bandeja-lechuga-mantecosa-lavada-sue-o-verde-x-200-gr/p",
-        "https://www.hiperlibertad.com.ar/tomate-perita-x-500g/p",
-        "https://www.hiperlibertad.com.ar/calabacin-anquito-fraccionado-x-700gr-1-2-calabacin/p",
-        "https://www.hiperlibertad.com.ar/mandarina-x-1kg/p",
-        "https://www.hiperlibertad.com.ar/naranja-p-jugo-x-1kg/p",
-        "https://www.hiperlibertad.com.ar/banana-x-kg-ecomm/p",
+        "https://www.hiperlibertad.com.ar/manzana-roja-fraccionada-x-1kg/p",
         "https://www.hiperlibertad.com.ar/pollo-fresco-x-un-aprox-2-5kg/p",
-        "https://www.hiperlibertad.com.ar/pulpa-de-jamon-de-cerdo-x-500-g/p",
-        "https://www.hiperlibertad.com.ar/milanesa-de-bola-de-lomo-de-novillito-x-500-g/p",
-        "https://www.hiperlibertad.com.ar/huevo-blanco-carnave-carton-x-12-unidades/p",
-        "https://www.hiperlibertad.com.ar/leche-entera-c-calcio-la-lacteo-larga-vida-1-l/p",
-        "https://www.hiperlibertad.com.ar/manteca-tonadita-200gr/p",
-        "https://www.hiperlibertad.com.ar/aceite-de-girasol-alsamar-x-900-ml/p",
-        "https://www.hiperlibertad.com.ar/brahma-chopp-can-4x6-473cc/p",
-        "https://www.hiperlibertad.com.ar/sal-fina-dos-anclas-250-gr/p",
-        "https://www.hiperlibertad.com.ar/mayonesa-hellmanns-clasica-x-475-gr/p",
-        "https://www.hiperlibertad.com.ar/cafe-molido-bonafide-sensaciones-torrado-intenso-500-gr/p",
-        "https://www.hiperlibertad.com.ar/yerba-playadito-suave-bcp-1kg/p",
+        "https://www.hiperlibertad.com.ar/salame-milan-lario-feteado-150g/p",
+        "https://www.hiperlibertad.com.ar/huevo-blanco-mediterranea-x6u/p",
+        "https://www.hiperlibertad.com.ar/leche-entera-la-seren-sima-mult-vit-3-sachet-x-1-lt/p",
+        "https://www.hiperlibertad.com.ar/queso-crema-cl-sico-casancrem-290gr-3/p",
+        "https://www.hiperlibertad.com.ar/yogur-bebible-vainilla-la-seren-sima-cl-sico-900gr-2/p",
+        "https://www.hiperlibertad.com.ar/manteca-primer-premio-100gr/p",
+        "https://www.hiperlibertad.com.ar/aceite-de-girasol-natura-1-5-lt/p",
+        "https://www.hiperlibertad.com.ar/sal-final-celusal-paquete-500-gr/p",
+        "https://www.hiperlibertad.com.ar/vinagre-vino-alcazar-x-500ml/p",
+        "https://www.hiperlibertad.com.ar/caf-cabrales-al-grano-torrado-molido-x-500-gr/p",
+        "https://www.hiperlibertad.com.ar/yerba-chamigo-sin-tacc-x-1-kg/p",
+        "https://www.hiperlibertad.com.ar/mayonesa-natura-250-cc/p"
     ]
 
 # 05 - Configuro la funcion Parse:
@@ -68,17 +60,19 @@ class ProductosSpider(scrapy.Spider):
             nombre_producto = response.css('span.vtex-store-components-3-x-productBrand::text').get()
 
             # El precio esta dividido en 2 span diferentes (enteros y decimales)
-            precio_entero = response.css("span.vtex-product-price-1-x-currencyInteger::text").getall()
-            precio_decimales =  response.css("span.vtex-product-price-1-x-currencyFraction::text").get()
+            precio_normal_oferta_entero = response.css("span.vtex-product-price-1-x-currencyInteger::text").getall()
+            precio_normal_oferta_decimales =  response.css("span.vtex-product-price-1-x-currencyFraction::text").get()
 
-            if precio_entero: # Si tiene un precio configurado
-              if len(precio_entero) == 1:  # Tiene solamente un preciosin ofertas
-                precio = f'{precio_entero[0]}, {precio_decimales}'
-              elif len(precio_entero[0])> 1: # cuando el producto tiene un precio anterior, enfatizando una oferta
-                precio = f'{precio_entero[1]}, {precio_decimales[1]}'  # Se toma el segundo valor, que es el precio real sin promocion.
-              else:
-                precio = f'{precio_entero[0]}.{precio_entero[1]}, {precio_decimales}' # cuando tiene entero y decimales
-            else: # Si no tiene un precion configurado
+            # Extraer precio desde el tag meta
+            precio_normal = response.css('meta[property="product:price:amount"]::attr(content)').get()
+
+            # Seleccionar el precio normal en caso de oferta, si no el precio  del tag meta
+
+            if precio_normal_oferta_entero:
+              precio = f'{ precio_normal_oferta_entero[0]}{precio_normal_oferta_entero[1]}, {precio_normal_oferta_decimales[1]}'
+            elif precio_normal:
+              precio = precio_normal.replace(".",",")
+            else:
               precio = "No disponible"
 
             yield {
